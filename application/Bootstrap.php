f73e13c7ad49f50286d459907606259d6fd6e79f
Reginaldo Castardo <rcastardo@gmail.com>
2014-06-09 09:48:01 -0300

    Changing value initSession

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -11,7 +11,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     public function _initSession()
     {
-        $session = new Zend_Session_Namespace('Blog');
+        $session = new Zend_Session_Namespace('System');
         Zend_Registry::set('session', $session);
     }
 
